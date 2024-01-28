f20fe356a5d9dd80b9714b4b9206d2eff5a3d605
Iain Schmitt <schm4131@umn.edu>
2024-01-28 15:55:51 -0600

    Fixing .sql syntax in dev DB population

--- state-economy-game-backend/populateDevDb.sql
+++ state-economy-game-backend/populateDevDb.sql
@@ -1,4 +1,4 @@
 create table TargetStateName (id integer primary key, targetStateName varchar(60), createdAt datetime, updatedAt datetime, targetStateGdp integer);
 create table GameIds (id varchar(36) PRIMARY KEY, attempts tinyint, createdAt datetime, updatedAt datetime);
 
-insert into TargetStateName (id, targetStateName) values (1, 'Rhode Island', 72772)
+insert into TargetStateName (id, targetStateName, targetStateGdp) values (1, 'Rhode Island', 72772);
