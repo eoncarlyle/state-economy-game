d78f25c017708aad425f7322a0238ee908dd44d1
Iain Schmitt <iain.schmitt@relexsolutions.com>
2024-02-05 16:06:19 -0600

    Revert "Revert "Moving from material ui icons to static SVGs""
    This reverts commit c9524bcc137568c508ff4946b26553874d66a262.

--- frontend/src/components/BearingIcon.tsx
+++ frontend/src/components/BearingIcon.tsx
@@ -1,14 +1,23 @@
-import { North, NorthEast, East, SouthEast, South, SouthWest, West, NorthWest } from "@mui/icons-material";
+
+import NorthIcon from "../assets/north.svg"
+import NorthEastIcon from "../assets/northEast.svg"
+import EastIcon from "../assets/east.svg"
+import SouthEastIcon from "../assets/southEast.svg"
+import SouthIcon from "../assets/south.svg"
+import SouthWestIcon from "../assets/southWest.svg"
+import WestIcon from "../assets/west.svg"
+import NorthWestIcon from "../assets/northWest.svg"
+
 
 export default function BearingIcon({ bearing }: { bearing: number}) {
   const cutoff = 45 / 2; 
-  if (0 <= bearing && bearing < cutoff) return <North />;
-  else if (cutoff <= bearing && bearing < 90 - cutoff) return <NorthEast />;
-  else if (90 - cutoff <= bearing && bearing < 90 + cutoff) return <East />;
-  else if (90 + cutoff <= bearing && bearing < 180 - cutoff) return <SouthEast />;
-  else if (180 - cutoff <= bearing && bearing < 180 + cutoff) return <South />;
-  else if (180 + cutoff <= bearing && bearing < 270 - cutoff) return <SouthWest />;
-  else if (270 - cutoff <= bearing && bearing < 270 + cutoff) return <West />;
-  else if (270 + cutoff <= bearing && bearing < 360 - cutoff) return <NorthWest />;
-  else return <North />;
+  if (0 <= bearing && bearing < cutoff) return <img src={NorthIcon} alt="North"/>;
+  else if (cutoff <= bearing && bearing < 90 - cutoff) return <img src={NorthEastIcon} alt="North East"/>;
+  else if (90 - cutoff <= bearing && bearing < 90 + cutoff) return <img src={EastIcon} alt="East"/>;
+  else if (90 + cutoff <= bearing && bearing < 180 - cutoff) return <img src={SouthEastIcon} alt="South East"/>;
+  else if (180 - cutoff <= bearing && bearing < 180 + cutoff) return <img src={SouthIcon} alt="South"/>;
+  else if (180 + cutoff <= bearing && bearing < 270 - cutoff) return <img src={SouthWestIcon} alt="South West"/>;
+  else if (270 - cutoff <= bearing && bearing < 270 + cutoff) return <img src={WestIcon} alt="West"/>;
+  else if (270 + cutoff <= bearing && bearing < 360 - cutoff) return <img src={NorthWestIcon} alt="North West"/>;
+  else return <img src={NorthIcon} alt="North"/>;
 }
