dff3313df0175877c5d2204d80a9a9741c01722a
Iain Schmitt <iain.schmitt@relexsolutions.com>
2024-02-07 11:36:41 -0600

    Restrict last 15 target state records from being selected

--- state-economy-game-backend/src/service.ts
+++ state-economy-game-backend/src/service.ts
@@ -20,10 +20,10 @@ import {
   getUsStateRecords,
 } from "state-economy-game-util/util";
 import { CheckedHttpError } from "state-economy-game-util/model";
-import { MAX_GUESSES } from "state-economy-game-util/constants";
+import { MAX_GUESSES, TARGET_STATE_RETENTION } from "state-economy-game-util/constants";
 import stateEconomies from "./stateEconomies";
 
-const getTargetStateRecord = async () => StateRecord.of(await getTargetStateName());
+const getTargetStateRecord = async () => StateRecord.of((await getTargetStateModel()).targetStateName);
 
 export const postGameId = async (_req: Request, res: Response) => {
   const newUUID = randomUUID();
@@ -35,7 +35,7 @@ export const postGameId = async (_req: Request, res: Response) => {
 };
 
 export const getTargetStateEconomy = async (_req: Request, res: Response, next: NextFunction) => {
-  const targetStateModel = await TargetStateModel.findOne();
+  const targetStateModel = await getTargetStateModel();
   if (!targetStateModel) throw new Error("Target state not found");
 
   const targetStateEconomy = getEconomyNode(targetStateModel.targetStateName);
@@ -59,7 +59,7 @@ export const postGuessSubmission = async (req: Request, res: Response, next: Nex
     return next(CheckedHttpError.of("Game id and a guess state name must both be valid", 422));
   } else if (gameId.attempts >= MAX_GUESSES) {
     return next(CheckedHttpError.of("Too many request have been made for this game", 422));
-  } else if ( requestTimestamp === gameId.lastRequestTimestamp) {
+  } else if (requestTimestamp === gameId.lastRequestTimestamp) {
     return next(CheckedHttpError.of("Duplicate of successful request", 422));
   }
 
@@ -123,39 +123,66 @@ export const handleUnhandledRoute = (req: Request, res: Response) => {
   res.status(404).json({ message: "HTTP response code 404: route not handled" });
 };
 
-const getTargetStateName = async () => {
-  const targetStateModel = await TargetStateModel.findOne();
+const getTargetStateModel = async () => {
+  const targetStateModel = await TargetStateModel.findOne({order: [['id', 'DESC']]});
   if (!targetStateModel) throw new Error("Target state not found");
-  return targetStateModel.targetStateName;
+  return targetStateModel;
 };
 
 export const runDailyTasks = async () => {
   const obsoleteDate = new Date();
   obsoleteDate.setDate(obsoleteDate.getDate() - 1);
 
-  const obsoleteIdCount = await GameIdModel.destroy({
+  const deletedGameIdCount = await GameIdModel.destroy({
     where: {
       createdAt: {
         [Op.lt]: obsoleteDate,
       },
     },
   });
-  console.log(`Obsolete GameIds removed: ${obsoleteIdCount}}`);
+  console.log(`Obsolete GameIds deleted: ${deletedGameIdCount}`);
 
-  await TargetStateModel.destroy({ truncate: true });
-  const newTargetState = getUsStateRecords().at(getRandomInt(getUsStateRecords().length));
+  deleteObsoleteTargetStates();
+
+  const unselectableTargetStateNames = (await TargetStateModel.findAll({ attributes: ["targetStateName"] })).map(
+    (targetStateModel: TargetStateModel) => targetStateModel.targetStateName
+  );
+  const selectableTargetStates = getUsStateRecords().filter((stateRecord: StateRecord) => !unselectableTargetStateNames.includes(stateRecord.name));
+  const newTargetState = selectableTargetStates.at(getRandomInt(selectableTargetStates.length)); 
   if (!newTargetState) return new Error("Failure to create new target state");
+
   const newEconomyNode = getEconomyNode(newTargetState.name);
   if (!newEconomyNode)
     return new Error(`Failure to find economy records for new target state '${newTargetState.name}'`);
 
   await TargetStateModel.create({
-    id: 1,
+    id: (await TargetStateModel.count()) + 1,
     targetStateName: newTargetState?.name,
     targetStateGdp: getRoundedTotalGdp(newEconomyNode),
   });
   console.log(`New target state: ${newTargetState?.name}`);
 };
+
+async function deleteObsoleteTargetStates(): Promise<void> {
+  const targetStateCount = await TargetStateModel.count();
+  // This makes an assumption that `id` values descend with row age
+  const obsoleteStateModelCount = targetStateCount - TARGET_STATE_RETENTION + 1;
+  if (obsoleteStateModelCount > 0) {
+    const deletedTargetStateCount = await TargetStateModel.destroy({
+      where: {
+        id: {
+          [Op.lte]: obsoleteStateModelCount,
+        },
+      },
+    });
+    console.log(`Obsolete target states deleted: ${deletedTargetStateCount}`);
+  
+    const updatedStateModelCount = await TargetStateModel.update(
+      { id: Sequelize.literal(`id - ${obsoleteStateModelCount}`) }, { where: {} }
+    )
+    console.log(`Updated target state rows: ${updatedStateModelCount}`)
+  }
+}
 
 function getRandomInt(max: number) {
   return Math.floor(Math.random() * max);
