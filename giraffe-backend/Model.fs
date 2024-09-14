d00a553c8afa4101c79b33b64689d7dd45f784ef
iwschmitt <iwschmitt@spscommerce.com>
2024-09-13 20:15:18 -0500

    WIP JSON serializing

--- giraffe-backend/Model.fs
+++ giraffe-backend/Model.fs
@@ -1,28 +1,37 @@
 namespace StateEconomyGame.Model
 
+open FSharp.Data
+
 type Coordinates = { Latitude: float; Longitude: float }
+
+type AppHttpError = { Code: int; Message: string }
+
+type AppResult<'a> = Result<'a, AppHttpError>
+
+type Guesses = //? Should this be destinct from other state data structures
+    { id: int
+      puzzleSession: string
+      stateName: string
+      createdAt: string
+      updatedAt: string } //! Need to make the non-option verions, this is getting tedious
 
 type StateRecord =
     { Name: string
       LatitudeN: float
       LongitudeW: float }
 
-type NonLeafEconomyNodeM = {
-    GdpCategory: string
-    Gdp: float
-}
+type NonLeafEconomyNode = { GdpCategory: string; Gdp: float }
 
-type LeafEconomyNode = {
-    GdpCategory: string
-    Children: List<EconomyNode>
-} and EconomyNode = Leaf of LeafEconomyNode | NonLeaf of NonLeafEconomyNodeM 
+type LeafEconomyNode =
+    { GdpCategory: string
+      Children: List<EconomyNode> }
 
-type StateEconomy = { }
-  
+and EconomyNode =
+    | Leaf of LeafEconomyNode
+    | NonLeaf of NonLeafEconomyNode
 
-type Guess =
-    { StateRecord: StateRecord
-      Distance: float
-      Bearing: float
-      PercentileScore: float }
+type StateEconomies = JsonProvider<"./stateEconomies.json">
 
+type LabeledStateEconomy = StateEconomies.Root
+
+type TargetState = { id: int; name: string; gdp: float }
--- giraffe-backend/Service.fs
+++ giraffe-backend/Service.fs
@@ -1,17 +1,88 @@
 module StateEconomyGame.Service
 
 open System
+open System.IO
+open System.Text.Json
+open System.Threading.Tasks
 open StateEconomyGame.Model
-open System.Data.SQLite
+open Dapper.FSharp.SQLite
+open Microsoft.Data.Sqlite
+open FSharp.Data
 
-let connection = new SQLiteConnection("Data Source=app.db")
+// Use Dapper instead, doesn't have the SQLite interop .dll issue
 
-let getTargetStateEconomy () = 0
+//! <-- Here, doing JSON serialisation
+
+let stateEconomies = StateEconomies.GetSamples()
+
+// H/T to Michael
+let taskMap<'a, 'b> (fn: 'a -> 'b) (a: Task<'a>) : Task<'b> =
+    task {
+        let! results = a
+        return fn results
+    }
+
+let connection =
+    use connection = new SqliteConnection("Data Source=app.sqlite")
+    OptionTypes.register () |> ignore
+    connection.Open() |> ignore
+    connection
+
+let targetStateTable = table'<TargetState> "target_states"
 
 let getTotalGdp (economyNode: EconomyNode) =
-    let rec loop economyNode = 
+    let rec loop economyNode =
         match economyNode with
         | NonLeaf nonLeaf -> nonLeaf.Gdp
         | Leaf leaf -> leaf.Children |> List.map loop |> List.sum
 
     loop economyNode |> Math.Round |> Convert.ToInt64
+
+let MgetTotalGdp (economyNode: StateEconomies.Root) =
+    let rec loop a =
+        match economyNode with
+        | LabeledStateEconomy le -> le.StateEconomy
+
+    loop economyNode |> Math.Round |> Convert.ToInt64
+
+
+let getTargetState () : Task<AppResult<TargetState>> =
+    select {
+        for targetState in targetStateTable do
+            selectAll
+            orderByDescending targetState.id
+    }
+    |> connection.SelectAsync<TargetState>
+    |> taskMap (fun result ->
+        match Seq.tryHead result with
+        | Some targetState -> Ok targetState
+        | None ->
+            Error
+                { Code = 500
+                  Message = "Internal error: target state not present" })
+
+let getEconomyNode (stateName: string) = //! Type inference legitamitely didn't work on this until I used the pipeline operator
+    match stateEconomies |> Array.filter (fun state -> state.Name = stateName) with
+    | [||] ->
+        Error
+            { Code = 500
+              Message = $"Internal Error: {stateName} not found!" }
+    | [| stateEconomy |] -> Ok stateEconomy
+    | _ ->
+        Error
+            { Code = 500
+              Message = "Internal error:" }
+
+
+let getTargetStateEconomy () : Task<AppResult<TargetState>> =
+
+    task {
+        let! targetState = getTargetState ()
+
+        let targetStateEconomy =
+            match targetState with
+            | Ok state -> getEconomyNode state.name |> Result.bind (fun node -> getTotalGdp node |> Ok)
+            | Error e -> Error e
+
+        return targetStateE
+    }
